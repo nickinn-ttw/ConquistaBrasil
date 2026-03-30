81e4d81221afb5da86c6d2fde9e39fb1de82955d
Enzo Leal <enzo@enzoleal.com>
2026-03-30 14:19:04 -0300

    Commit Barra de Pesquisa

---
+++ ConquistaBrasil/src/Componentes/barraPesquisa/barraPesquisa.jsx
@@ -0,0 +1,26 @@
+const BarraPesquisa = ({ busca, setBusca }) => {
+
+  const digitarBusca = (event) =>{
+    setBusca(event.target.value)
+  }
+  return (
+    <header className="header-container">
+      <div className="logo-area">
+        <h1>🏆 Campeões Brasileiros</h1>
+        <span>2000 à 2025</span>
+      </div>
+      
+      <div className="busca-area">
+        <input
+          type="text"
+          placeholder="Pesquisar clube..."
+          value={busca}
+          onChange= {digitarBusca}
+        />
+        <p>{busca}</p>
+      </div>
+    </header>
+  );
+};
+
+export default BarraPesquisa;
