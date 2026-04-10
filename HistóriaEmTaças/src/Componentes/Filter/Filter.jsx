640d10f4943776e837d74ef70d6b9524b6d27829
JuniorOliveira323 <aldenirabreu869@gmail.com>
2026-04-09 21:59:53 -0300

    Filter Working + UseState

---
+++ HistóriaEmTaças/src/Componentes/Filter/Filter.jsx
@@ -0,0 +1,21 @@
+const Filter = ({ filtroRegiao, setFiltroRegiao }) => {
+const [,] = useState("Todas");
+  
+  return (
+    <div className="filtros-container">
+      <label>Filtrar por Região: </label>
+      <select> 
+        value={filtroRegiao} 
+        onChange={(e) => setFiltroRegiao(e.target.value)}
+      
+        <option value="Todas">Todas as Regiões</option>
+        <option value="Sudeste">Sudeste</option>
+        <option value="Sul">Sul</option>
+        <option value="Nordeste">Nordeste</option>
+        <option value="Centro-Oeste">Centro-Oeste</option>
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
